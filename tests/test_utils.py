bf70612677bae40e663a46d6eb94720c44a6fe65
BriceSop <bradleysop@gmail.com>
2026-01-07 14:03:49 +0100

    Correcting test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@ import os
 
 import joblib
 import pandas as pd
+import pytest
 from utils.common import (
     create_directory,
     load_data_from_csv,
@@ -11,6 +12,14 @@ from utils.common import (
 )
 
 
+# Fixture and csv data generation
+@pytest.fixture(scope="session")
+def csv_data(tmp_path_factory):
+    data = pd.DataFrame({"Test":[1,2,3]})
+    fn = tmp_path_factory.mktemp("data") / "tmp_data.csv"
+    data.to_csv(fn, index=False)
+    return fn
+
 def test_load_yaml():
     # GIVEN a yaml file's path
     # WHEN load_yaml is applied
@@ -19,19 +28,19 @@ def test_load_yaml():
     assert cfg is not None
     assert type(cfg) is dict
 
-def test_load_data_from_csv():
+def test_load_data_from_csv(csv_data):
     # GIVEN a csv file's path
     # WHEN load_data_from_csv is applied
-    data = load_data_from_csv("data/raw/train.csv")
+    data = load_data_from_csv(csv_data)
     # THEN data should be a not empty DataFrame
     assert type(data) is pd.DataFrame
     assert data.empty is False
 
 def test_save_data_to_csv(tmp_path):
-    # GIVEN a filepath to save data into
-    # WHEN save_data_to_csv is applied
+    # GIVEN a data to save and a filepath
     data = pd.DataFrame({"Test":[1,2,3]})
     filename = 'dummy.csv'
+    # WHEN save_data_to_csv is applied
     save_data_to_csv(data, tmp_path, filename)
     filepath = os.path.join(tmp_path, filename)
     # THEN filepath should exist
